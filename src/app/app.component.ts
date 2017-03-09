b1f1ce336245cc307f3eb28d3af54ebd5c1d5848
Alex Schweiz <askibinskiy@gmail.com>
2017-03-09 16:00:18 +0200

    Initial maps install

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app works!';
+  lat: number = 46.485722;
+  lng: number = 30.743444;
+}
